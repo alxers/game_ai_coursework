4725d41fd30e61a08ff399366fbed3745133b524
Alexey Ershov <alex4rom@gmail.com>
2021-08-01 22:20:08 +0300

    Redraw on screen

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#include "shapes.c"
 
 // Make mapping so that it's possible to use RGB in Xlib
 unsigned long _rgb(int r, int g, int b) {
@@ -22,6 +23,17 @@ unsigned long _rgb(int r, int g, int b) {
 
 int w_width = 800; // 400
 int w_height = 600; // 200
+int gameTime = 600000; // 1000000 = 1 sec
+int randTime = 3000000;
+
+int result;
+struct timespec tp;
+clockid_t clk_id;
+
+//  clk_id = CLOCK_REALTIME;
+clk_id = CLOCK_MONOTONIC;
+//  clk_id = CLOCK_BOOTTIME;
+//  clk_id = CLOCK_PROCESS_CPUTIME_ID;
 
 // Xlib variables
 Display *disp;
@@ -57,26 +69,31 @@ void drawCircle() {
     XFillArc(disp, win, DefaultGC(disp, screen), 50-(30/2), 100-(30/2), 30, 30, 0, 360*64);
 }
 
-void draw() {
-    for (int j = w_height - 1; j >= 0; --j) {
-        for (int i = 0; i < w_width; ++i) {
-            int ir = 0;
-            int ig = 0;
-            int ib = 0;
-            // Set colors
-            XSetForeground(disp, DefaultGC(disp, screen), _rgb(ir, ig, ib));
-            // Actually draw a pixel at (i, j) coords
-            // TODO: check if XDrawPoints() will be faster
-            // in that case prepare array of points
+// void draw() {
+//     for (int j = w_height - 1; j >= 0; --j) {
+//         for (int i = 0; i < w_width; ++i) {
+//             int ir = 0;
+//             int ig = 0;
+//             int ib = 0;
+//             // Set colors
+//             XSetForeground(disp, DefaultGC(disp, screen), _rgb(ir, ig, ib));
+//             // Actually draw a pixel at (i, j) coords
+//             // TODO: check if XDrawPoints() will be faster
+//             // in that case prepare array of points
 
-            // We use "height - j" because otherwise the image would be flipped vertically
-            XDrawPoint(disp, win, DefaultGC(disp, screen), i, w_height - j);
-        }
-    }
-}
+//             // We use "height - j" because otherwise the image would be flipped vertically
+//             XDrawPoint(disp, win, DefaultGC(disp, screen), i, w_height - j);
+//         }
+//     }
+// }
 
 
 int main(void) {
+    // int clock_gettime(clockid_t clk_id, struct timespec *tp);
+    result = clock_gettime(clk_id, &tp);
+    printf("result: %i\n", result);
+    printf("tp.tv_sec: %lld\n", tp.tv_sec);
+    printf("tp.tv_nsec: %ld\n", tp.tv_nsec);
     // X window setup
     XEvent event;
 
@@ -92,30 +109,42 @@ int main(void) {
                               );
     XSelectInput(disp, win, ExposureMask | KeyPressMask);
     XMapWindow(disp, win);
+
+    XEvent evt;
+    evt.type = Expose;
+    evt.xexpose.type = Expose;
+    evt.xexpose.serial = 0;
+    evt.xexpose.send_event = 1;
+    evt.xexpose.window = win;
+    evt.xexpose.x = 0;
+    evt.xexpose.y = 0;
+    evt.xexpose.width = 400;
+    evt.xexpose.height = 400;
+    evt.xexpose.count = 0;
     // end X window setup
 
+
     while (1) {
+        // XClearWindow(disp, win);
+        randTime -= 100;
+
+        // drawRect();
+        // if (randTime > 0) {
+        //     drawCircle();
+        //     printf("%d\n", randTime);
+        //     // XClearWindow(disp, win);
+        // }
+
         XNextEvent(disp, &event);
         if (event.type == Expose) {
-          // draw();
-          drawRect();
-          drawCircle();
-        }
+            drawCircle();
 
-        if (event.type == KeyPress) {
-            if (event.xkey.keycode == W_KEY) {
-                // draw();
+            if (randTime == 0) {
+                drawRect();
+                printf("%d\n", randTime);
             }
-            if (event.xkey.keycode == A_KEY) {
-            }
-            if (event.xkey.keycode == S_KEY) {
-            }
-            if (event.xkey.keycode == D_KEY) {
-            }
-
-            if (event.xkey.keycode == Q_KEY) {
-                break;
-            }
+            // Send "Expose" event again in order to redraw
+            XSendEvent(disp, win, 0, ExposureMask, &evt);
         }
     }
 
---
+++ shapes.c
@@ -0,0 +1,13 @@
+struct rect
+{
+	int width;
+	int height;
+	int lifetime;
+};
+
+struct circle
+{
+	int radius;
+	int lifetime;
+	int type;
+};
